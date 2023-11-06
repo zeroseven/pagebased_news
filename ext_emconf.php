e376d83be4c5299901baccf8a069213b650a353b
Raphael Thanner <r.thanner@zeroseven.de>
2023-11-06 21:09:40 +0100

    Update extension title

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Article',
+    'title' => 'News pages',
     'description' => 'Create news articles as "normal" pages in TYPO3. Easy to install and ready to use straight away.',
     'category' => 'plugin',
     'author' => 'Raphael Thanner',
