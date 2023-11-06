45fec3178ba5e28ccb23083a9f5aadcf4fa2770b
Raphael Thanner <r.thanner@zeroseven.de>
2023-11-06 15:21:22 +0100

    Update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
-            'pagebased' => ''
+            'typo3' => '11.5.0-12.4.99',
+            'pagebased' => '1.2.0-1.99.99'
         ]
     ]
 ];
